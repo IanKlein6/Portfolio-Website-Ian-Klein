4600e9e6cb78782a002dcec26588fa69e575d874
IanKlein6 <ianklein66@gmail.com>
2024-01-03 12:48:24 +0100

    header upadated

--- frontend/src/container/header/Header.jsx
+++ frontend/src/container/header/Header.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+
 import { AppWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
 const Header = () => (
   <div className="app__header app__flex">
+
+    {/* Info Badges */}
     <motion.div
       whileInView={{ x: [-100, 0], opacity: [0, 1] }}
       transition={{ duration: 0.5 }}
@@ -25,21 +39,31 @@ const Header = () => (
           <p className="p-text">Freelancer</p>
         </div>
       </div>
+
+      
     </motion.div>
 
+    {/* Language Squares */}
+    <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className="app__header-circles"
+    >
+      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
+    </motion.div>
+    
+    {/* Profile Picture */}
     <motion.div
       whileInView={{ opacity: [0, 1] }}
-      transition={{ duration: 0.5, delayChildren: 0.5 }}
+      transition={{ duration: 1.2, ease: 'easeInOut' }}
       className="app__header-img"
     >
       <img src={images.profile} alt="profile_bg" />
-      <div className="header-circles-container">
-        {[images.flutter, images.redux, images.sass].map((circle, index) => (
-          <div className="circle-cmp app__flex" key={`circle-${index}`}>
-            <img src={circle} alt="circle_bg" />
-          </div>
-        ))}
-      </div>
+      
     </motion.div>
   </div>
 );
